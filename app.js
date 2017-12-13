f4c106aefa61e665eef5a698a38552f3f2176761
[ianizaguirre] <ian@izaguir.re>
2017-12-13 16:06:46 -0500

    refactor: deleted a bunch of code and removed the views folder and removed the index.js router file etc -> Removed all of this because Express is our API backend so we did not have to keep a bunch of stuff that dealt only with VIWES since Views is going to be handled by our Angular front-end side

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 const express      = require('express');
 const path         = require('path');
-const favicon      = require('serve-favicon');
 const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
@@ -13,12 +12,6 @@ require("./config/mongoose-setup");
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// default value for title local
-app.locals.title = 'Typify';
 
 
 
@@ -46,10 +39,6 @@ app.use(
 
 // ============Routers===================================
 
-const index = require('./routes/index');
-app.use('/', index);
-
-
 const articleApi = require('./routes/article-api-router');
 app.use('/api', articleApi);
 
@@ -64,22 +53,5 @@ app.use('/api', userApi);
 
 
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
