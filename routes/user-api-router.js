a7ae0fc08a5c5e3b3873acebdc242e1e6cdfb6b4
[ianizaguirre] <ian@izaguir.re>
2017-12-18 14:04:14 -0500

    Still working on user-router

--- routes/user-api-router.js
+++ routes/user-api-router.js
@@ -1,6 +1,6 @@
 "use strict";
 const express = require('express');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user-model');
 
@@ -10,54 +10,54 @@ const router = express.Router();
 
 // POST /api/signup
 router.post('/signup', (req, res, next) => {
-	// if (req.body.password === undefined ||
-	// 		req.body.password.length < 16   ||
-	// 		req.body.password.match(/[^a-z0-9]/i) === null
-	// 		) {
-	// 			res.status(400).json({ error: 'Password invalid' });
-	// 			return;
-	// }
+	if (req.body.password === undefined ||
+			req.body.password.length < 5   ||            // CHANGED FROM '16' FOR NOW 
+			req.body.password.match(/[^a-z0-9]/i) === null
+			) {
+				res.status(400).json({ error: 'Password invalid' });
+				return;
+	}
 
-	// User.findOne({ username: req.body.username })
-	// 	.then((userFromDb) => {
-	// 		if (userFromDb !== null) {
-	// 			res.status(400).json({ error: 'Username is taken' });
-	// 			return;
-	// 		}
+	User.findOne({ username: req.body.username })
+		.then((userFromDb) => {
+			if (userFromDb !== null) {
+				res.status(400).json({ error: 'Username is taken' });
+				return;
+			}
 
-	// 		const salt = bcrypt.genSaltSync(10);
-	// 		const scrambledPassword = bcrypt.hashSync(req.body.password, salt);
+			const salt = bcrypt.genSaltSync(10);
+			const scrambledPassword = bcrypt.hashSync(req.body.password, salt);
 
-// 			const theUser = new User({
-// 				fullName: req.body.fullName,
-// 				username: req.body.username,
-// 				encryptedPassword: scrambledPassword
-// 			});
-// 				return theUser.save();
-// 		})
-// 		.then((userFromDb) => {
-// 			// log the user in automatically if sign up works
-// 			req.login(userFromDb, (err) => {
-// 				// clear the "encryptedPassword" before sending the user info
-// 				// ( otherwise its a security risk )
-// 				userFromDb.encryptedPassword = undefined;
+			const theUser = new User({
+				fullName: req.body.fullName,
+				username: req.body.username,
+				encryptedPassword: scrambledPassword
+			});
+				return theUser.save();
+		})
+		.then((userFromDb) => {
+			// log the user in automatically if sign up works
+			req.login(userFromDb, (err) => {
+				// clear the "encryptedPassword" before sending the user info
+				// ( otherwise its a security risk )
+				userFromDb.encryptedPassword = undefined;
 			
-// 					res.status(200).json({
-// 					isLoggedIn: true,	
-// 					userInfo:	userFromDb
-// 					});
-// 			});
-// 		})
-// 		.catch((err) => {
-// 			if (err.errors) {
-// 				console.log('POST /singup ERROR! ');
-// 				console.log(err);
-// 				res.status(400).json(err.errors);
-// 			}
-// 			else {
-// 				res.status(500).json({ error: 'Sign up database error' });
-// 			}
-// 		});
+					res.status(200).json({
+					isLoggedIn: true,	
+					userInfo:	userFromDb
+					});
+			});
+		})
+		.catch((err) => {
+			if (err.errors) {
+				console.log('POST /singup ERROR! ');
+				console.log(err);
+				res.status(400).json(err.errors);
+			}
+			else {
+				res.status(500).json({ error: 'Sign up database error' });
+			}
+		});
 
 }); // POST /signup
 
@@ -66,7 +66,7 @@ router.post('/signup', (req, res, next) => {
 
 
 
-// // POST /api/login
+ // POST /api/login
 
 // router.post('/login', (req,res, next) => {
 // 	User.findOne({ username: req.body.username })
@@ -109,7 +109,7 @@ router.post('/signup', (req, res, next) => {
 
 
 
-// // DELETE /api/logout
+// DELETE /api/logout
 
 
 // router.delete('/logout', (req, res, next) => {
